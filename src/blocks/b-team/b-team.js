8bd9f0c10ae559b15fafd80c6651a443ae3fefe9
Michael <michlenanosoft@gmail.com>
2016-07-15 12:30:38 +0500

    done first page

--- src/blocks/b-team/b-team.js
+++ src/blocks/b-team/b-team.js
@@ -1,11 +1,11 @@
 $(function () {
-    $('.b-gallery__list').slick({
-        arrows: true,
-        dots: true,
+    $('.b-team__list').slick({
+        //arrows: true,
+        //dots: false,
         infinite: true,
         swipe: false,
         //centerMode: true,
         //centerPadding: '60px',
-        slidesToShow: 1
+        slidesToShow: 3
     });
 });
